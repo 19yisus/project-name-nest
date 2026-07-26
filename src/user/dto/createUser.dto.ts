92d09b1fe6669747b6e653bf3271f10921481263
19yisus <jesusitomorales70@gmail.com>
2026-07-26 14:45:45 -0400

    d

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -2,17 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({
-    description: 'The name of the user',
-    example: 'John Doe',
-  })
+  @ApiProperty()
   @IsString()
   @MinLength(3)
   name!: String;
-  @ApiProperty({
-    description: 'The email of the user',
-    example: 'john.doe@example.com',
-  })
+  @ApiProperty()
   @IsEmail()
   @MinLength(10)
   email!: String
